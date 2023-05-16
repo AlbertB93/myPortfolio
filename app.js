5d95b224b08609801b2e7960ecf6bce82bd20dcf
AlbertB93 <bercik93@interia.eu>
2023-05-16 14:54:35 +0200

    Added some elements to projects section.

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-window.onscroll = function () { myFunction() };
-let header = document.querySelector("header")
-let nav = document.querySelector("nav")
-let ul = document.querySelector("ul")
+window.onscroll = function () {
+  myFunction();
+};
+let header = document.querySelector("header");
+let nav = document.querySelector("nav");
+let ul = document.querySelector("ul");
 
 function myFunction() {
-    if (document.body.scrollTop > 600|| document.documentElement.scrollTop > 600) {
-        header.classList.add("changePostionToRight")
-        nav.classList.add("displayColumn")
-        ul.classList.add("changeSize")
-    } else {
-
-        header.classList.remove("changePostionToRight")
-        nav.classList.remove("displayColumn")
-    }
+  if (
+    document.body.scrollTop > window.innerHeight ||
+    document.documentElement.scrollTop > window.innerHeight
+  ) {
+    header.classList.add("changePostionToRight");
+    nav.classList.add("displayColumn");
+    ul.classList.add("changeSize");
+  } else {
+    header.classList.remove("changePostionToRight");
+    nav.classList.remove("displayColumn");
+  }
 }
